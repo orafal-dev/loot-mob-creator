eeedadc8373fc5dea3f32534193cd046913f1e41
Rafal Olszewski <olszak94@gmail.com>
2026-07-10 16:25:28 +0200

    style: update paragraph styling and add horizontal rule in HomePage component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,11 @@ export default function HomePage() {
   return (
     <BackgroundGradient>
       <h1>Generate loot</h1>
-      <p className="lead">
+      <p className="text-sm text-muted-foreground">
         Use fields below to enter monster name, and generate loot from Tibia
         Fandom Wiki
       </p>
+      <hr className="my-4" />
       <WebScrape />
     </BackgroundGradient>
   );
